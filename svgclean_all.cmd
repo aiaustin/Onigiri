755b34e067f3d8f8bc1c3d13000f0f8df9d3cbc9
Zaher Dirkey <zaherdirkey@yahoo.com>
2025-06-02 21:34:35 +0300

    Fix outlines of SVG icons

--- svgclean_all.cmd
+++ svgclean_all.cmd
@@ -1,11 +1,18 @@
-rem @echo off
+@echo off
 rem --https://github.com/RazrFalcon/svgcleaner-- no it remove color name "black"
 rem use https://github.com/scour-project/scour/
-cd Onigiri\icons\dark
+cd Onigiri\icons\light
 rem for %%f in (*.svg) do svgcleaner "%%f" "%%f" --keep-named-ids
-ren %%f %%f.svg
-for %%f in (*.svg) do scour -i %%f.svg %%f --enable-comment-stripping --shorten-ids --indent=none --disable-simplify-colors --strip-xml-prolog --remove-titles --no-line-breaks
-cd ..\light
-ren %%f %%f.svg
-for %%f in (*.svg) do scour -i %%f.svg %%f --enable-comment-stripping --shorten-ids --indent=none --disable-simplify-colors --strip-xml-prolog --remove-titles --no-line-breaks
+
+for %%f in (*.svg) do (
+    ren %%f %%f.temp
+    scour -i %%f.temp %%f --enable-comment-stripping --shorten-ids --indent=none --disable-simplify-colors --strip-xml-prolog --remove-titles --no-line-breaks
+    del %%f.temp
+)
+
+for %%f in (*.svg) do (
+    ren %%f %%f.temp
+    scour -i %%f.temp %%f --enable-comment-stripping --shorten-ids --indent=none --disable-simplify-colors --strip-xml-prolog --remove-titles --no-line-breaks
+    del %%f.temp
+)
 pause
